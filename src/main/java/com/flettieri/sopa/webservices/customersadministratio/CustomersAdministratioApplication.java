ec6b14545154b33240739688e9537c3befd00612
lett-java <fabiolettieri9@gmail.com>
2021-02-02 16:03:20 -0300

    Projeto Criado

---
+++ src/main/java/com/flettieri/sopa/webservices/customersadministratio/CustomersAdministratioApplication.java
@@ -0,0 +1,13 @@
+package com.flettieri.sopa.webservices.customersadministratio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersAdministratioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersAdministratioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flettieri/sopa/webservices/customersadministratio/CustomersAdministratioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flettieri.sopa.webservices.customersadministratio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersAdministratioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
